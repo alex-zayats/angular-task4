2b2c353befc4054ecb9c95a82c4bc11ca72b22ef
Alex <alex3953898@yandex.com>
2016-02-01 01:27:54 +0200

    added axes

--- app/controllers.js
+++ app/controllers.js
@@ -7,10 +7,17 @@ app.controller('DashboardCtrl', [ '$scope', '$timeout', function($scope, $timeou
     var countElements = 15;
 
     $scope.elements = [];
-    
+    $scope.labelsX = [];
+    $scope.labelsY = [];
+
     for (var i = 0; i < countElements; i++) {
         $scope.elements.push({ x: ($scope.fieldWidth / countElements)*i+25, y: Math.random() * $scope.fieldHeight });
+        $scope.labelsY.push({ y: ($scope.fieldHeight / countElements)*(countElements-i), value: Math.round(($scope.fieldHeight*10 / countElements)*i/10)/10 });
+        $scope.labelsX.push({ x: ($scope.fieldWidth / countElements)*i, value: Math.round(($scope.fieldWidth*10 / countElements)*i/10)/10 });
     }; 
+
+    $scope.labelsY.push({ y: 0 , value: $scope.fieldHeight/10 });
+    $scope.labelsX.push({ x: $scope.fieldWidth, value: $scope.fieldWidth/10 });
 
     $scope.path = "M0 " + $scope.fieldHeight;
     for (var i = 0; i < countElements; i++) {
